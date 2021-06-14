f1a1e8da59e9ddebe2c573bb2f14bb7fbd09e3e1
francois2sap <francois@desaporta.fr>
2021-06-14 16:52:00 +0200

    ex05

--- ex05/find_evil.sh
+++ ex05/find_evil.sh
@@ -1,1 +1,1 @@
-awk -F"|" '($3 >= 216){print}' log_terminal_CDG.csv > evil_corp_council_members.csv
+awk -F"|" '($3 >= 216) && ($3 <= 222){print}' log_terminal_CDG.csv > evil_corp_council_members.csv
